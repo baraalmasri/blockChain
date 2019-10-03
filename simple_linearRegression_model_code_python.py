052a3ee3ec7a45a73c7b99a002312ffd64fe3d0c
Baraa Masri <15340030+baraalmasri@users.noreply.github.com>
2019-10-03 10:40:33 -0700

    Update simple_linearRegression_model_code_python.py

--- simple_linearRegression_model_code_python.py
+++ simple_linearRegression_model_code_python.py
@@ -6,10 +6,11 @@
    
    dataset = pd.read_csv('Salary_Data.csv') 
    #importing datasets 
-   #here simpley we chouse the x colum and the other side we chouse the y colums 
+   #here simpley we choose the x column and the other side we choose the y columns 
    x= dataset.iloc[:,:-1].values 
    y= dataset.iloc[:,1].values 
    #splitting the dataset into training set and test set 
+   #datalari ayirma veri madencilik te setleri %75 egitiyoruz %25 test olarak aliyoruz
    x_train,x_test,y_train,y_test =train_test_split(x,y,test_size=1/3,random_state=0 ) 
    
    #fitting simple linear regression to the training set 
@@ -32,7 +33,7 @@
    y'ler gercek datanin grafgi ''' 
    plt.scatter(x_train,y_train ,color='red') 
    '''burada  grafigi ciziyoruz x'ler gercek datanin x'leri 
-   y'ler ise egitlimis  datanin grafgi ''' 
+   y'ler ise egitlimis  datanin grafigi ''' 
    plt.scatter(x_train,regressor.predict(x_train) ,color='blue') 
    plt.title('salary vs experience (training set)') 
    plt.xlabel('year of experience') 
